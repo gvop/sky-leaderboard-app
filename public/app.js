13f0debeb45c8a5d829db8063c5df19dfa6c929a
gvop <guusvanooijen@gmail.com>
2016-03-04 12:39:00 +0000

    Pushing older version

--- public/app.js
+++ public/app.js
@@ -8,15 +8,3 @@ requests.getReq = function(url){
 
 requests.getReq("api/programmes")
 
-$("#uploadForm").submit(function(event){
-  event.preventDefault();
-  console.log(event.target.image.value)
-  $.ajax({
-    type: "POST",
-    url: "/upload",
-    data: data,
-    success: success,
-    dataType: dataType
-  });
-})
-
